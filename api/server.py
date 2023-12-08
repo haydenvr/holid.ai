ede1a6e8f21e22fbbe962f970a6307bf0d5e1da3
haydenvr <h.v.reyswoud@gmail.com>
2023-12-07 20:13:01 -0800

    remove old file

--- api/server.py
+++ api/server.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from backend-imagegen.ImageGen import ImageGen
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def homepage():
     return 'Hello, World!'
+
+# add api call for get image
+@app.route('/api/get_image')
+def get_image():
+    return 'Hello, World!
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
