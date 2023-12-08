437e83f46a438a6dbe5740e45da71af30f297d00
haydenvr <h.v.reyswoud@gmail.com>
2023-12-07 21:11:26 -0800

    update server to return the image via api

--- api/server.py
+++ api/server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from requests import Response
 from imagegen import generate_image
 
 app = Flask(__name__)
@@ -10,7 +11,9 @@ def homepage():
 # add api call for get image
 @app.route('/api/get_image')
 def get_image():
-    return generate_image("A cute cat", "UPDATE-KEY", test=True)
+    file = generate_image("A cute cat", "UPDATE-KEY", test=True)
+    return Response(file.read(), mimetype='image/png') 
+# upload file to caller
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
--- imagegen/imagegen.py
+++ imagegen/imagegen.py
@@ -6,8 +6,9 @@ import os
 api_key = "UPDATE-KEY"
 def generate_image(text, api_key, test = True, engine_id="stable-diffusion-v1-6", api_host="https://api.stability.ai"):
         if test:
-            return f'./out/v1_txt2img_0.png'
+            return open("./imagegen/out/v1_txt2img_0.png", "r")
         
+        print("Generating image")
         response = requests.post(
             f"{api_host}/v1/generation/{engine_id}/text-to-image",
             headers={
@@ -34,11 +35,7 @@ def generate_image(text, api_key, test = True, engine_id="stable-diffusion-v1-6"
 
         data = response.json()
 
-        for i, image in enumerate(data["artifacts"]):
-            with open(f"./out/v1_txt2img_{i}.png", "wb") as f:
-                f.write(base64.b64decode(image["base64"]))
-
-        return f"./out/v1_txt2img_{i}.png"
+        return base64.b64decode(data["artifacts"][0]["base64"])
 
 if __name__ == "__main__":
     print(generate_image("A cute cat", api_key))
